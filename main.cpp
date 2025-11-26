5dec42f3c36568674b8736f0f6bce37c7ab47bd3
emilianodiaz01 <emiliano.diaz@utec.edu.pe>
2025-11-25 21:59:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,16 @@ int main() {
     cout << "'" << caja.getSimbolo() << "'\n";
 
     CTablero tablero(6);
-    tablero.mostrar();
+    CTablero tablero2(10);
+    cout << endl;
+    //tablero.movimiento(6,5,5,5);//vertical
+    //tablero.mostrar();
+    //tablero.movimiento(6,5,6,6);//horizontal
+    //tablero.mostrar();
+    tablero2.movimiento(10,9,10,10); //horizontal
+    //tablero2.mostrar();
+    tablero2.movimiento(10,9,10,10); //vertical
+    tablero2.mostrar();
 
 
     return 0;
